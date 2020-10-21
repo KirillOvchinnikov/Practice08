d0194aa2aafbf98a922601575e215ec0c0160955
KirillOvchinnikov <kirill.ovchinnikov.1968@mail.ru>
2020-10-21 23:13:42 +0300

    Initial commit

---
+++ src/Pr08/UnfairWaitList.java
@@ -0,0 +1,18 @@
+package Pr08;
+
+public class UnfairWaitList<E> extends WaitList<E> {
+    public UnfairWaitList(){
+        super();
+    }
+    void remove(E element){
+        if(content.peek()!=element) content.remove(element);
+    }
+    void moveToBack(E element){
+          if(contains(element)){
+              content.add(element);
+              content.remove();
+          }else{
+              content.add(element);
+          }
+    }
+}
